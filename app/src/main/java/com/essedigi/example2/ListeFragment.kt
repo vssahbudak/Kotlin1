9c25796e536127b09c6dde4abcf554e720dcf7e9
vssahbudak <vssahbudak@hotmail.com>
2024-08-07 17:17:04 +0300

    kotlin 1

---
+++ app/src/main/java/com/essedigi/example2/ListeFragment.kt
@@ -0,0 +1,28 @@
+package com.essedigi.example2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ListeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_liste, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
